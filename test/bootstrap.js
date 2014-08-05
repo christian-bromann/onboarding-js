41f25629cdd32a99ecf9dc7efc201d8dd399ca92
christian-bromann <contact@christian-bromann.com>
2014-08-04 17:04:22 -0700

    fixed capabilities setup

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -23,8 +23,6 @@ if(process.env.TRAVIS_BUILD_ID) {
     capabilities.port = 80;
     capabilities.user = 'cb-onboarding';
     capabilities.key  = 'ffd251ca-2705-49fc-a824-50333dc99eeb';
-    capabilities.desiredCapabilities.platform = process.env._PLATFORM.replace(/_/, ' ');
-    capabilities.desiredCapabilities.version  = process.env._VERSION;
     capabilities.desiredCapabilities.tags = ['js', process.env._BROWSER];
     capabilities.desiredCapabilities.name = 'onboarding test';
 }
@@ -36,6 +34,9 @@ if(process.env._MOBILETYPE) {
     capabilities.desiredCapabilities['appium-version']  = process.env._APPIUMVERSION;
     capabilities.desiredCapabilities.tags.push('appium');
     capabilities.desiredCapabilities.tags.push(process.env._MOBILETYPE);
+} else {
+    capabilities.desiredCapabilities.platform = process.env._PLATFORM.replace(/_/, ' ');
+    capabilities.desiredCapabilities.version  = process.env._VERSION;
 }
 
 /**
