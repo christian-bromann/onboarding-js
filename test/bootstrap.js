da849e1c697b0c83f46bf89088750089a8719f6c
christian-bromann <contact@christian-bromann.com>
2014-08-04 17:56:07 -0700

    set app capability in native mode

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -18,25 +18,30 @@ capabilities.desiredCapabilities = {
     browserName: process.env._BROWSER
 }
 
-if(process.env.TRAVIS_BUILD_ID) {
+if (process.env.TRAVIS_BUILD_ID) {
     capabilities.host = 'ondemand.saucelabs.com';
     capabilities.port = 80;
     capabilities.user = 'cb-onboarding';
-    capabilities.key  = 'ffd251ca-2705-49fc-a824-50333dc99eeb';
+    capabilities.key = 'ffd251ca-2705-49fc-a824-50333dc99eeb';
     capabilities.desiredCapabilities.tags = ['js', process.env._BROWSER];
     capabilities.desiredCapabilities.name = 'onboarding test';
 }
 
-if(process.env._MOBILETYPE) {
+if (process.env._MOBILETYPE) {
     capabilities.desiredCapabilities.platformVersion = process.env._PLATFORMVERSION;
     capabilities.desiredCapabilities.platformName = process.env._PLATFORMNAME;
     capabilities.desiredCapabilities.deviceName = process.env._DEVICENAME.replace(/_/, ' ');
-    capabilities.desiredCapabilities['appium-version']  = process.env._APPIUMVERSION;
+    capabilities.desiredCapabilities['appium-version'] = process.env._APPIUMVERSION;
     capabilities.desiredCapabilities.tags.push('appium');
     capabilities.desiredCapabilities.tags.push(process.env._MOBILETYPE);
+
+    if (process.env._APP) {
+        capabilities.desiredCapabilities.deviceName = process.env._APP;
+    }
+
 } else {
     capabilities.desiredCapabilities.platform = process.env._PLATFORM.replace(/_/, ' ');
-    capabilities.desiredCapabilities.version  = process.env._VERSION;
+    capabilities.desiredCapabilities.version = process.env._VERSION;
     capabilities.desiredCapabilities.tags.push('webdriver');
 }
 
